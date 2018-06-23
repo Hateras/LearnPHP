6f3fb692b48224983fb8cb0698cf95392322d373
Hateras <hateras@hotmail.com>
2018-06-23 21:12:03 +0300

    PHP Functions

---
+++ php/Subroutines/Subroutines.php
@@ -0,0 +1,43 @@
+#!/usr/bin/php
+<?php
+#a global variable
+$globalvar = 5;
+
+#double the param, global variable's value is NOT modified, no result is returned
+function DoubleTheParamNoResult($val) {
+    echo "Param: $val\n";
+    $val *= 2;
+    echo "Double the param: $val\n";
+}
+
+#double the param, global variable's value is NOT modified, result is doubled value
+function DoubleTheParam($val) {
+    $val *= 2;
+    return $val;
+}
+
+#modify globa variables
+function DoubleGlobals() {
+    global $globalvar, $val;
+    $globalvar *= 2;
+    $val *= 2;
+}
+
+#main script
+#another global variable
+$val = 3;
+echo "Initial value: $val\n";
+echo "Result of DoubleTheParamNoResult: " . DoubleTheParamNoResult($val) . "\n";
+echo "Value after function call: $val\n";
+
+echo "Function result: " . DoubleTheParam($val) . "\n";
+echo "Value after function call: $val\n";
+
+#global variables
+echo "Initial value of globalvar: $globalvar\n";
+echo "Initial value of val: $val\n";
+DoubleGlobals();
+echo "Value of globalvar after function call: $globalvar\n";
+echo "Value of val after function call: $val\n";
+
+?>
